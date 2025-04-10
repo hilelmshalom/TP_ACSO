432825ae93e07dca6bfa8e5a5e367dd5394d37bc
hilelmshalom <hsalomon@udesa.edu.ar>
2025-04-10 12:28:12 -0300

    - dev ej2

--- TP2-x86_64/ej1/ej1.asm
+++ TP2-x86_64/ej1/ej1.asm
@@ -19,7 +19,33 @@ extern str_concat
 
 
 string_proc_list_create_asm:
+    # Prologue
+    pushq   %rbp
+    movq    %rsp, %rbp
 
+    # Allocate memory: sizeof(string_proc_list) = 16 (2 pointers)
+    movl    $16, %edi       # size argument for malloc
+    call    malloc          # call malloc, result in %rax
+
+    testq   %rax, %rax      # check if malloc returned NULL
+    je      .return_null    # if NULL, return NULL
+
+    # Initialize list->first = NULL
+    movq    $0, (%rax)
+
+    # Initialize list->last = NULL
+    movq    $0, 8(%rax)
+
+    # Return pointer to newly created list
+    jmp     .return
+
+.return_null:
+    movq    $0, %rax        # return NULL
+
+.return:
+    # Epilogue
+    popq    %rbp
+    ret
 string_proc_node_create_asm:
 
 string_proc_list_add_node_asm:
